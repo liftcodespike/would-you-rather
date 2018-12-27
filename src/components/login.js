8902413cd116bd3a3c6f0bbe689fc1e4fc665901
Ray Bell <iamraybell@gmail.com>
2018-12-26 19:21:30 -0800

    error handling on login is completed.

--- src/components/login.js
+++ src/components/login.js
@@ -17,9 +17,9 @@ const loginBoxStyle= {
     alignItems: `center`,
     textAlign: `center`,
     margin: `auto`,
-    height: `75vh`,
+    height: `40vh`,
     width: `60vw`,
-    minHeight: `220px`,
+    minHeight: `270px`,
     backgroundColor: `white`,
     borderRadius: 10,
     textAlign: `center`,
@@ -34,49 +34,84 @@ const input = {
     margin: 5,
 
 }
+
 const buttonStyle = {
     border: `thin solid black`,
     height: `30px`,
     margin: 5,
 }
 
+const errorField = {
+    color: `red`,
+}
+
 export class Login extends React.Component{
     state = {
         nameValue: '',
         ageValue: '',
-        hasErrors: false,
+        errors:[],
 
     }
 
-    handleFieldChange(value, field){
-        
+    handleNameChange(value){
+        const errors = []
+        if (value.length < 3) {
+            errors.push('Name is too short.')
+        }
+        if (value > 20) {
+            errors.push('Name is too long.')
+        }
         this.setState((prevState)=>{
             console.log(value)
             return {
                 ...prevState,
-                [field]: value
+                nameValue: value,
+                errors: errors,
             }
         })
     }
+
+    handleAgeChange(value){
+        const reg = new RegExp('^\\d+$');
+        const errors = []
+        if (!reg.test(value)) {
+            errors.push('The age entered is not a valid number.')
+        }
+
+        this.setState((prevState)=>{
+            console.log(value)
+            return {
+                ...prevState,
+                ageValue: value,
+                errors: errors,
+            }
+        })
+    }
+
     render(){
         return (
             <div style = {overlayStyle} >
                 <div style ={loginBoxStyle}>
                     <h2>Login here to use this site.</h2>
+                        {
+                            this.state.errors.map((error)=>{
+                                return <div style={ errorField} key={error}>{error}</div>
+                            })
+                        }
                     <form>
                         <input 
                             style={input} 
                             type='text' 
                             value={this.state.nameValue} 
                             placeholder='Enter your name here'
-                            onChange={(e)=> this.handleFieldChange(e.target.value, 'nameValue')}
+                            onChange={(e)=> this.handleNameChange(e.target.value)}
                         /><br></br>
                         <input 
                             style={input} 
                             type='number' 
                             value={this.state.ageValue} 
                             placeholder='Enter your age here'
-                            onChange={(e)=> this.handleFieldChange(e.target.value, 'ageValue')}
+                            onChange={(e)=> this.handleAgeChange(e.target.value)}
                         /><br></br>
                         <button style ={buttonStyle} >Submit</button>
                     </form>
