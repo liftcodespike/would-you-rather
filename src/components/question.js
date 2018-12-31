b440cfd75fc717f30378b6a118b22cb8b249897e
Ray Bell <iamraybell@gmail.com>
2018-12-31 13:14:08 -0800

    Worked more on the question page.

--- src/components/question.js
+++ src/components/question.js
@@ -1,21 +1,33 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import { NavBar } from './navbar';
 
 const questionWrapper = {
-    height: `300px`,
+    padding:5,
+    minHeight: `300px`,
     width: `70vw`,
     minWidth: 420,
     margin: `auto`,
     marginTop: `10px`,
     border: `thin solid black`,
+    borderRadius: 8,
+    boxShadow: `5px 10px #888888`,
+}
+const btnStyle = {
+    borderRadius: 0,
+    border: `none`,
+    backgroundColor: `#eb3a94`,
+    color: `white`,
+    padding: 8,
 }
 
 const imageWrapper = {
     display: `inline-block`,
-    height: `280px`,
+    minHeight: `280px`,
     verticalAlign: `top`,
-    width: `20%`,
-    border: `thin solid black`,
+    width: `25%`,
+    border: `none`,
+    borderRadius: 8,
     padding: 10,
 }
 const resultsWrapper = {
@@ -25,19 +37,31 @@ const resultsWrapper = {
     padding: 10,
     fontSize: `17px`,
 }
+const cardHeader={
+    borderBottom: 'thin solid #eb3a94'
+}
+const voteStyle= {
+    color: '#eb3a94'
+}
 
 export class Question extends React.Component{
+    votedStr=`You voted for the above option.`
 
-    state = {
-
-        answered: this.checkIfAnswered(),
+    getVotedTxtOrVoteBTN (optionNum){
+        if (this.checkIfAnswered()) {
+            if(this.props.user.answers[this.props.question.id]=== optionNum){
+                return <p style={voteStyle}><sup>*</sup>{this.votedStr}</p>
+            }
+            return;
+        }
+        return <button style={btnStyle}>Click To vote for the above option.</button>
     }
 
     checkIfAnswered(){
-        console.log(this.props.user.answers.hasOwnProperty(this.props.question.id) );
+        return this.props.user.answers.hasOwnProperty(this.props.question.id);
     }
 
-    answered(){
+    setProfileLayout(){
         const questionCreator = this.props.users[this.props.question.author]
         const totalVotes = this.props.question.optionOne.votes.length + this.props.question.optionTwo.votes.length
         return (
@@ -56,12 +80,21 @@ export class Question extends React.Component{
                         <p><b>Asked by: {questionCreator.name}</b></p>
                     </div>
                     <div style={resultsWrapper} >
-                        
-                        <h1>Results</h1>
-                        <p>{this.props.question.optionOne.text}</p>
-                        <p><b>Voted By:</b> {this.props.question.optionOne.votes.length} of {totalVotes}</p>
-                        <p>{this.props.question.optionTwo.text}</p>
-                        <p><b>Voted By:</b> {this.props.question.optionTwo.votes.length} of {totalVotes}</p>
+                        <div style={cardHeader}> 
+                            <h1>Results</h1>
+                        </div>
+                        <p><i>{this.props.question.optionOne.text}</i></p>
+                        <p>
+                            <b>Voted By: </b>
+                            {this.props.question.optionOne.votes.length} of {totalVotes} 
+                        </p>
+                        {this.getVotedTxtOrVoteBTN('optionOne')}
+                        <p><i>{this.props.question.optionTwo.text}</i></p>
+                        <p>
+                            <b>Voted By:</b> 
+                            {this.props.question.optionTwo.votes.length} of {totalVotes} 
+                        </p>
+                        {this.getVotedTxtOrVoteBTN('optionTwo')}
                     </div>
                 </div>
             </div>
@@ -69,6 +102,9 @@ export class Question extends React.Component{
     }
 
     render() {
-        return this.answered();
+        if(!this.props.user){
+            return <Redirect to='/' />
+        }
+        return this.setProfileLayout();
     }
 }
